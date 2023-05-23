6905b9adefb4f0264b16463c2bdd7b3dcf21e196
Randy Lopez <rlopez48@ucsc.edu>
2023-05-23 11:54:46 -0700

    updated frontend 5/22

--- script.js
+++ script.js
@@ -3,31 +3,31 @@ var fileInput = document.getElementById('file-input');
 var uploadBtn = document.getElementById('upload-btn');
 var fileName = document.getElementById('file-name');
 
-dropzone.addEventListener('dragover', function(e) {
+dropzone.addEventListener('dragover', function(e) {//when dragging files over
   e.preventDefault();
   dropzone.style.backgroundColor = '#e9e9e9';
 });
 
-dropzone.addEventListener('dragleave', function(e) {
+dropzone.addEventListener('dragleave', function(e) {//when leaving field with file?
   e.preventDefault();
-  dropzone.style.backgroundColor = '#fff';
+  dropzone.style.backgroundColor = '#ffe6f2';
 });
 
-dropzone.addEventListener('drop', function(e) {
+dropzone.addEventListener('drop', function(e) {//showing files when dropped into box
   e.preventDefault();
-  dropzone.style.backgroundColor = '#fff';
+  dropzone.style.backgroundColor = '#e7dcf5';
 
   var files = e.dataTransfer.files;
   displayFiles(files);
 });
 
-uploadBtn.addEventListener('click', function() {
+uploadBtn.addEventListener('click', function() {//gets file input
   fileInput.click();
 });
 
 fileInput.addEventListener('change', function() {
-  var files = fileInput.files;
-  displayFiles(files);
+  var files = fileInput.files;//gets files from input
+  displayFiles(files);//runs displayFiles function
 });
 
 function displayFiles(files) {
@@ -36,6 +36,10 @@ function displayFiles(files) {
     var file = files[i];
     fileNames += file.name + ', ';
   }
-  fileName.textContent = 'Selected files: ' + fileNames.slice(0, -2);
+  fileNames.textContent = 'Selected files: ' + fileNames.slice(0, -2);
+  var fileNamePrint = document.getElementById("file-name")
+  fileNamePrint.textContent += fileNames;
+  console.log(fileNamePrint)
+  console.log("Hello world")
   // Perform actions with the selected file(s) here
 }
